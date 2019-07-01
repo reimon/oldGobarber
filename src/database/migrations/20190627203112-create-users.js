1f7c1a713fcdab45b6baba3f0f548eae792fbebe
REIMON FERREIRA GONÇALVES <reimonf@gmail.com>
2019-07-01 13:33:31 -0300

    fim modulo02

--- src/database/migrations/20190627203112-create-users.js
+++ src/database/migrations/20190627203112-create-users.js
@@ -1,11 +1,42 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('users', {
-      id: Sequelize.INTERGER,
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      provider: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      uptated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     });
   },
 
-  down: (queryInterface, Sequelize) => {
+  down: queryInterface => {
     return queryInterface.createTable('users');
   },
 };
